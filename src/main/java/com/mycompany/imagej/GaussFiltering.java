b07f38e198e5ba789f2dc432596235195836cb6d
Skullriver <alinanov257@gmail.com>
2023-01-26 22:13:10 +0100

    translation of python code

--- src/main/java/com/mycompany/imagej/GaussFiltering.java
+++ src/main/java/com/mycompany/imagej/GaussFiltering.java
@@ -30,6 +30,7 @@ import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.gui.GenericDialog;
+import ij.gui.StackWindow;
 import ij.io.Opener;
 import ij.process.ByteProcessor;
 import ij.process.ImageConverter;
@@ -38,9 +39,17 @@ import ij.process.ShortProcessor;
 
 import org.ejml.data.DMatrixRMaj;
 import org.ejml.data.DMatrixSparse;
+import org.ejml.data.DMatrixSparseCSC;
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.dense.row.SingularOps_DDRM;
 import org.ejml.dense.row.factory.DecompositionFactory_DDRM;
+import org.ejml.factory.DecompositionFactory;
+import org.ejml.factory.LinearSolver;
+import org.ejml.factory.LinearSolverFactory;
+import org.ejml.factory.QRDecomposition;
 import org.ejml.interfaces.decomposition.SingularValueDecomposition;
 import org.ejml.interfaces.decomposition.SingularValueDecomposition_F64;
+import org.ejml.ops.CommonOps;
 import org.ejml.simple.SimpleMatrix;
 import org.ejml.simple.SimpleSVD;
 
@@ -100,6 +109,10 @@ public class GaussFiltering<T extends RealType<T>> implements Command {
      * @throws Exception
      */
     private static double tau = 230;
+    private static int k = 2;
+    private static int rank = 3;
+    private static double tol = 0.001;
+    private static int power = 5;
     private static MODE mode = MODE . SOFT ;
     private enum MODE { SOFT , HARD ; }
     private static int height, width, nbSlices, type ;
@@ -164,23 +177,160 @@ public class GaussFiltering<T extends RealType<T>> implements Command {
             double[][][] matrix = constructMatrix(imp.getStack(), type) ;
             
             /*
-             * SVD but it's not what we want
+             * SVD 
              */
             SimpleMatrix A = new SimpleMatrix(matrix[0]);
+                     
+            SimpleSVD<SimpleMatrix> svd = A.svd();
+
+            SimpleMatrix U=null,W=null,V=null, check = null;
             
-            SimpleSVD<SimpleMatrix> s = A.svd();
-            SimpleMatrix U=null,W=null,V=null;
-            U=s.getU();
-            W=s.getW();
-            V=s.getV();
+			double [] Svd = null;
+            Svd = svd.getSVD().getSingularValues();
+            U=svd.getU();
+            W=svd.getW();
+            V=svd.getV(); 
+            SimpleMatrix X = svd.getU().extractMatrix(0, U.numRows(), 0, k);
+            SimpleMatrix Y = svd.getV().extractMatrix(0, k, 0, V.numCols());
+            SimpleMatrix s = svd.getW().extractMatrix(0, k, 0, k);
+            
+            X = X.mult(s);
+            SimpleMatrix L = X.mult(Y);
+            
+            SimpleMatrix S = A.minus(L);
+            
+            //thresholding
+            S = thresholding(S);        
+            
+            double result = (double) rank / k;
+            int rankk = (int) Math.round(result);
+            SimpleMatrix error = new SimpleMatrix(rank * power, 1);
+            SimpleMatrix T = (A.minus(L)).minus(S);
+            double normD = A.normF();
+            double normT = T.normF();
+            
+            error.set(0, (double) normT / normD);
+            
+            int iii = 1;
+            boolean stop = false;
+            double alf = 0;
+            
+            for(int i=0; i<rankk; i++) {
+            	int rrank = rank;
+            	int est_rank = 1;
+            	alf = 0;
+            	double increment = 1;
+
+            	if (iii == power * (i - 2) + 1) {
+            		iii = iii + power;
+            	}
+            	
+            	for(int j=1; j<power+1; j++) {
+            		
+            		//update X 
+            		//TODO: absolute values 
+            		X = (L.mult(Y.transpose()));
+            		
+            		DenseMatrix64F X2 = X.getMatrix();
+            		
+            		// Do a QR decomposition
+                    QRDecomposition<DenseMatrix64F> qr = DecompositionFactory.qr(X.numRows(), X.numCols());
+                    qr.decompose(X2);
+                    DenseMatrix64F qm = qr.getQ(null, true);
+                    DenseMatrix64F rm = qr.getR(null, true);
+                    
+                    X = SimpleMatrix.wrap(qm);
+            
+                    // Update of Y
+                    Y = (X.transpose()).mult(L);
+                    L = X.mult(Y);
+                    
+                    // Update of S
+                    T = A.minus(L);
+                    //thresholding
+                    S = thresholding(T);
+                    
+                    // Error, stopping criteria
+                    T = T.minus(S);
+                    int ii = iii + j - 1;
+                    
+                    normT = T.normF();
+                    
+                    error.set(ii, (double) normT / normD);
+                    
+                    if (error.get(ii) < tol) {
+                    	stop = true;
+                    	break;
+                    }
+                        
+                    if (rrank != rank) {
+                    	rank = rrank;
+                    	if(est_rank == 0) {
+                    		alf = 0;
+                    		continue;
+                    	}
+                    }
+                    
+                    // Adjust alf
+                    double ratio = (double) error.get(ii) / error.get(ii-1);
+                    
+                    // Intermediate variables
+                    SimpleMatrix X1 = X;
+                    SimpleMatrix Y1 = Y;
+                    SimpleMatrix L1 = L;
+					SimpleMatrix S1 = S;
+                    SimpleMatrix T1 = T;
+                    
+                    
+                    if (ratio >= 1.1) {
+                    	increment = Math.max(0.1 * alf, 0.1 * increment);
+                    	X = X1;
+                        Y = Y1;
+                        L = L1;
+                        S = S1;
+                        T = T1;
+                        error.set(ii, error.get(ii-1));
+                        alf = 0;
+                    }else if (ratio > 0.7) {
+                    	increment = Math.max(increment, 0.25 * alf);
+                        alf = alf + increment;
+                    }
+                    
+                    // Update of L
+                    X1 = X;
+                    Y1 = Y;
+                    L1 = L;
+                    S1 = S;
+                    T1 = T;
+                    L = L.plus(T.scale(1+alf));
+  
+            	}
+            	
+            	if( stop == true) {
+            		break;
+            	}
+
+            }
+            L = X.mult(Y);
+           
+            
+            double[][] L2 = matrix2Array(L);
+            double[][] S2 = matrix2Array(S);
         	
+            ImagePlus im = new ImagePlus (" Background Image ", constructImageStack (
+            		L2, type ) ) ;
+            ImagePlus im2 = new ImagePlus (" Sparse Image ", constructImageStack (
+            		S2, type ) ) ;
+            
+            im.show();
+            im2.show();
             
             /*
              * Maybe will be useful
              */
             
             // load the dataset
-//            final Dataset dataset = ij.scifio().datasetIO().open(file.getPath());
+            final Dataset dataset = ij.scifio().datasetIO().open(file.getPath());
 //            
 //            Img img = dataset.getImgPlus().getImg();
 //            
@@ -249,5 +399,46 @@ public class GaussFiltering<T extends RealType<T>> implements Command {
     	}
     	return matrix ;
     }
+    
+    private static ImageStack constructImageStack ( double [][] matrix , int bit ) {
+    	ImageStack newStack = new ImageStack ( width , height ) ;
+    	ByteProcessor bp = new ByteProcessor ( width , height ) ;
+    	for ( int i = 0; i < width ; i ++)
+    		for ( int j = 0; j < height ; j ++)
+    			bp.putPixel (i , j , ( int ) matrix[ i ][ j ]) ;
+    	newStack . addSlice ( bp ) ;
+    	
+    	return newStack;
+    }
+    private static double[][] matrix2Array(SimpleMatrix matrix) {
+        double[][] array = new double[matrix.numRows()][matrix.numCols()];
+        for (int r = 0; r < matrix.numRows(); r++) {
+            for (int c = 0; c < matrix.numCols(); c++) {
+                array[r][c] = matrix.get(r, c);
+            }
+        }
+        return array;
+    }
+    
+    private static SimpleMatrix thresholding(SimpleMatrix S) {
+    	for (int i = 0; i < S.numRows(); i++) {
+            for (int j = 0; j < S.numCols(); j++) {
+            	double val = S.get(i, j);
+                if (val > tau) {
+                	S.set(i, j, val - tau);
+                } else if (val < -tau) {
+                	S.set(i, j, val + tau);
+                } else {
+                	S.set(i, j, 0);
+                }
+            }
+        }
+        return S;
+    }
+   
+
+    
+    
+    
 
 }
